eb1c72f61f588358b634f8e361e63b58432d6437
Gilberto Bragga <gilbertobragga@outlook.com>
2025-06-13 18:18:16 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,10 +5,25 @@ let items = container.querySelectorAll('.list .item')
 let indicator = document.querySelector('.indicators')
 let dots = indicator.querySelectorAll('ul li')
 
+let active = 0
+let firstPosition = 0
+let lastPosition = items.length - 1
+
 nextButton.onclick = () => {
-    console.log("Botão next")
+    let itemOld = container.querySelector('.list .item.active')
+    itemOld.classList.remove('active')
+
+    active = active + 1 > lastPosition ? 0 : active + 1
+    items[active].classList.add('active')
+
+    let dotsOld = indicator.querySelector('ul li.active')
+    dotsOld.classList.remove('active')
 }
 
 prevButton.onclick = () => {
-    console.log("Botão prev")
+    let itemOld = container.querySelector('.list .item.active')
+    itemOld.classList.remove('active')
+
+    active = active - 1 < firstPosition ? lastPosition : active - 1
+    items[active].classList.add('active')
 }
